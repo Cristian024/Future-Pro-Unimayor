dcb630ba80831a09c9557e06fc3adc5883a5d7fa
Cristian024 <dleonrobledo@gmail.com>
2024-07-31 20:25:49 -0500

    refactor(ContactCard.vue): user name in contact card

--- src/common/cards/ContactCard.vue
+++ src/common/cards/ContactCard.vue
@@ -11,8 +11,7 @@
 <template>
     <div class="contact-div flex mt-[10px] w-[100%] h-[50px] items-center cursor-pointer p-[10px] transition-[0.2s] rounded-[0.5rem]" v-if="user != null">
         <img :src="user.img_profile" class="h-[25px] w-[25px] rounded-[100%] mr-[10px]">
-        <p v-if="user.role === 'student'">{{ user.first_name }} {{ user.last_name }}</p>
-        <p v-else-if="user.role === 'enterprise'">{{ user.name }}</p>
+        <p>{{ user.name }}</p>
     </div>
 </template>
 
